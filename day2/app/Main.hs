9198a95f797b67b15e4ab9da2afd3f97a9f4c67e
Asante-ntiro <asante.ntiro@gmail.com>
2023-09-19 22:41:42 +0300

    1st three :zap:

--- day2/app/Main.hs
+++ day2/app/Main.hs
@@ -1,4 +1,13 @@
 module Main where
+import qualified Data.List as parseLine
+
+data Moves = Rock | Paper | Scissors deriving (Show, Eq, Ord)
+
+toMove :: String -> Move
+toMove "A" = Rock
+toMove "B" = Paper
+toMove "C" = Scissors
+
 
 main :: IO ()
 main = putStrLn "Hello, Haskell!"
